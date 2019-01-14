5d37661cdb8e008006e8b55ba9ac5cc56f985a79
AndileNgubane <mthembungubane@gmail.com>
2019-01-14 14:40:34 +0200

    file change

--- Absa.DateAccess/RolesPermission.cs
+++ Absa.DateAccess/RolesPermission.cs
@@ -24,8 +24,10 @@ namespace Absa.DateAccess
         public string Type { get; set; }
         public Nullable<System.DateTime> DateLogged { get; set; }
         public string Description { get; set; }
+        public Nullable<int> UserId { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<User> Users { get; set; }
+        public virtual User User { get; set; }
     }
 }
